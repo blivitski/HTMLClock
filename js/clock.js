06e0220ea124599dbe5dd02313ea258bf520ba91
Brandon Livitski <blivitsk@csclnx03.ad.calpoly.edu>
2015-02-05 21:43:53 -0800

    lab5 first commit

--- js/clock.js
+++ js/clock.js
@@ -35,3 +35,53 @@ function getTemp() {
    }
 });
 }
+
+function showAlarmPopup() {
+   $("#mask").removeClass("hide");
+   $("#popup").removeClass("hide");
+}
+
+function hideAlarmPopup() {
+   $("#mask").addClass("hide");
+   $("#popup").addClass("hide");
+}
+
+function insertAlarm(hours, mins, ampm, alarmName) {
+   var blankDiv = $("<div>");
+   blankDiv.addClass("flexable");
+   var innerDiv1 = $("<div>");
+   innerDiv1.addClass("name");
+   innerDiv1.html(alarmName + "-");
+   var innerDiv2 = $("<div>");
+   innerDiv2.addClass("time");
+   innerDiv2.html(hours + ":" + mins + ampm);
+   blankDiv.append(innerDiv1);
+   blankDiv.append(innerDiv2);
+   $("#alarms").append(blankDiv);
+}
+
+function addAlarm() {
+   var hours = $("#hours option:selected").text();
+   var mins = $("#mins option:selected").text();
+   var ampm = $("#ampm option:selected").text();
+   var alarmName = $("#alarmName").val();
+   insertAlarm(hours, mins, ampm, alarmName);
+   hideAlarmPopup();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
